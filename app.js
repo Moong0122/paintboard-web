4e29cc958c0c5e1f8c1afe7588e0d577df490fbb
Moong0122 <2015004802@hanyang.ac.kr>
2019-12-25 21:03:09 +0900

    drawing line on painting board -> CanvasRenderingContext2D

---
+++ app.js
@@ -0,0 +1,38 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function onMouseDown(event) {
+  painting = true;
+}
+
+function stopPainting(event) {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+// canvas가 존재하는지 확인해준다!
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
