38cafd385bb09d411508d0fad1b60eb91d571bc0
Moong0122 <2015004802@hanyang.ac.kr>
2019-12-28 20:44:39 +0900

    change brush thickness and color && full board coloring

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const canvas = document.getElementById("jsCanvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("jsCanvas"),
+  ctx = canvas.getContext("2d"),
+  colors = document.getElementsByClassName("jsColor"),
+  range = document.getElementById("jsRange"),
+  mode = document.getElementById("jsMode");
 
-ctx.strokeStyle = "#2c2c2c";
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function onMouseMove(event) {
   const x = event.offsetX;
@@ -18,10 +26,6 @@ function onMouseMove(event) {
   }
 }
 
-function onMouseDown(event) {
-  painting = true;
-}
-
 function stopPainting(event) {
   painting = false;
 }
@@ -29,10 +33,50 @@ function stopPainting(event) {
 function startPainting() {
   painting = true;
 }
+
+function handleCanvasClick() {
+  if (filling) ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+
 // canvas가 존재하는지 확인해준다!
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+Array.from(colors).forEach(color =>
+  color.addEventListener("click", handleColorClick)
+);
+
+// range가 정의 되어있지 않을 수도 있기 때문에 if문으로 확인을 먼저 해준다.
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
 }
