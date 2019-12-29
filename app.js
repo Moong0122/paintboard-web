265e697653020dd63a6bcbcd0fc577d2c9ede1cd
Moong0122 <2015004802@hanyang.ac.kr>
2019-12-29 17:37:56 +0900

    we can save image in our computer

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const canvas = document.getElementById("jsCanvas"),
   ctx = canvas.getContext("2d"),
   colors = document.getElementsByClassName("jsColor"),
   range = document.getElementById("jsRange"),
-  mode = document.getElementById("jsMode");
+  mode = document.getElementById("jsMode"),
+  saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
 
+// 캔버스의 초기 배경화면 색(흰색) 지정해주기
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -68,6 +72,14 @@ function handleModeClick() {
   }
 }
 
+function handleSaveClick() {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "paintJS[🎨]";
+  link.click();
+}
+
 Array.from(colors).forEach(color =>
   color.addEventListener("click", handleColorClick)
 );
@@ -79,4 +91,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
